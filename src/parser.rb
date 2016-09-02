8dfe1f358c0835e587b8f32516af890e7ae41615
Simon Weiler <simon@die-weilers.de>
2016-09-02 17:58:52 +0200

    Added some simple usable behaviour to count group members

--- src/parser.rb
+++ src/parser.rb
@@ -1,15 +1,33 @@
 require 'csv'
+
 class Parser
-  def self::parse(input_str)
+  def self.parse(input_str)
+    table = CSV.parse(input_str, csv_options)
+    return table_to_array(table)
+  end
+
+  def self.parse_file(filename)
+    table = CSV.read(filename, csv_options)
+    return table_to_array(table)
+  end
+
+  private
+  def self.table_to_array(table)
     res = []
-    table = CSV.parse(input_str,
-                      :headers => true,
-                      :converters => :numeric,
-                      :header_converters => [CSV::HeaderConverters[:symbol]])
+
     table.each do |row|
       row_hash = row.to_hash
       res.push(row_hash)
     end
     return res
   end
+
+  def self.csv_options
+    {
+      :headers => true,
+      :converters => :numeric,
+      :header_converters => lambda {|s| s.to_sym}
+    }
+  end
+
 end
